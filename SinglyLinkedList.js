4aea0d2a14b60fd070ab030a8e87d25338b2ebac
Anastasia <slyadneva.anastasia@gmail.com>
2020-03-10 19:07:24 -0500

    get, set, insert, and remove

--- SinglyLinkedList.js
+++ SinglyLinkedList.js
@@ -12,6 +12,7 @@ class SinglyLinkedList {
     this.length = 0 
   }
 
+  // add an item at the end 
   push(val) {
     let newNode = new Node(val); 
 
@@ -30,6 +31,7 @@ class SinglyLinkedList {
     return this; 
   }
 
+  // remove an item from the end
   pop () {
     if (!this.head) {
       return undefined
@@ -65,6 +67,124 @@ class SinglyLinkedList {
       // return the item node that was popped off 
       return current
     }
+  }
+
+  // removing an item from the beginning (aka removing the head) and making the next item the head 
+  // O(1) time compared to arrays because no need to re-index everything else in the list 
+  shift () {
+    if (!this.head) {
+      return undefined
+    } else {
+      let currentHead = this.head; 
+      this.head = currentHead.next; 
+      this.length --; 
+
+      if (this.length === 0) {
+        this.tail = null; 
+      }
+
+      return currentHead; 
+    }
+  }
+
+  // adding a node to the beginning of the list 
+  // aka setting the new node as the new head and moving the current head down one position
+  unshift (val) {
+    let newHead = new Node(val); 
+
+    if (!this.head) {
+      this.head = newHead; 
+      this.tail = newHead;
+    } else {
+      newHead.next = this.head; 
+      this.head = newHead;       
+    }
+
+    this.length ++; 
+    return this; 
+  }
+
+  // returns the value of the element at the passed in index 
+  // need to traverse starting at the head up to the index and return the value 
+  get (index) {
+
+    if (index < 0 || index >= this.length) {
+      return null
+    } else {
+      let counter = 0; 
+      let current = this.head;
+
+      while(counter !== index) {       
+        current = current.next
+        counter++           
+      }
+
+      return current; 
+    }    
+  }
+
+  set (index, value) {
+    let nodeToSet = this.get(index); 
+    if (nodeToSet) {
+      nodeToSet.val = value; 
+      return true 
+    } 
+
+    return false 
+  }
+
+  insert (index, value) {
+    if(index < 0 || index > this.length) {
+      return false 
+    } else if (index === this.length) {
+      this.push(value); 
+      return true 
+    } else if (index === 0) {
+      this.unshift(value); 
+      return true
+    }
+    // if none of the above are true, it means the given index is in the middle of the list 
+    else {
+      // define preceeding and current nodes
+      // aka the inserted element will go between these two nodes 
+      let preceedingNode = this.get(index-1);
+      let currentNode = preceedingNode.next; 
+
+      // create the node to insert 
+      let nodeToInsert = new Node(value); 
+
+      // set the next property of the node we want insert to the current node 
+      nodeToInsert.next = currentNode; 
+
+      //set the next property of the preceeding node to the node we want to insert 
+      preceedingNode.next = nodeToInset; 
+
+      this.length ++; 
+
+      return true; 
+    }
+  }
+
+  // the remove method accepts an index and removes the value at that index 
+  remove (index) {
+    if (index < 0 || index > this.length) {
+      return undefined
+    } 
+    if (index === this.length - 1) {
+      return this.pop()
+    }
+    if (index === 0) {
+      return this.shift()
+    }
+
+    let preceedingNode = this.get(index-1);
+    let nodeToRemove = preceedingNode.next; 
+    let suceedingNode = nodeToRemove.next; 
+
+    preceedingNode.next = suceedingNode; 
+
+    this.length --; 
+    return nodeToRemove; 
   }
 
 }
