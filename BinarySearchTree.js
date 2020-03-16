a30feaee47e9087a91416d24a17fd9cc2af3bad6
Anastasia <slyadneva.anastasia@gmail.com>
2020-03-16 14:33:18 -0500

    tree BF traversal and binary search tree insert and contains methods

---
+++ BinarySearchTree.js
@@ -0,0 +1,97 @@
+class Node {
+  constructor (value) {
+    this.value = value; 
+    this.left = null;
+    this.right = null; 
+  }
+}
+
+class BinarySearchTree {
+  constructor () {
+    this.root = null; 
+  }
+
+  insert (value) {
+    let newNode = new Node(value)
+
+    if (!this.root) {
+      this.root = newNode; 
+      return this; 
+    } else {
+      let current = this.root; 
+
+      while (true) {
+        if (value === current.value) {
+          return undefined; 
+        }
+        if (value < current.value) {
+          if (!current.left) {
+            current.left = newNode;
+            return this; 
+          } else {
+            current = current.left;
+          }
+        } 
+        
+        else if (value > current.value) {
+          if (!current.right) {
+            current.right = newNode;
+            return this; 
+          } else {
+            current = current.right; 
+          }
+        }
+      }
+    }
+  }
+
+  contains (value) {
+    if (!this.root) {
+      return false
+    } 
+
+    let current = this.root; 
+    let isFound = false; 
+
+    while (current && isFound === false) {
+      if (value < current.value) {
+        current = current.left
+      }
+      else if (value > current.value) {
+        current = current.right
+      }
+      else if (value === current.value) {
+        isFound = true
+      }
+    }
+
+    return isFound
+  }
+
+  BFS () {
+    // this is what is returned at the end with all the traversed values 
+    let data = [];
+
+    // this helps keep track of the values to be traversed 
+    let queue = [];
+
+    let node = this.root; 
+
+    queue.push(node); 
+
+    while(queue.length) {
+      // remove FIRST element from the queue and return is as node variable 
+      node = queue.shift(); 
+      data.push(node.value); 
+
+      if (node.left) {
+        queue.push(node.left)
+      }
+      if (node.right) {
+        queue.push(node.right)
+      }
+    }
+
+    return data
+  }
+}
--- Tree.js
+++ Tree.js
@@ -11,5 +11,28 @@ class Node {
   remove (data) {
     this.children = this.children.filter(child => child.data !== data);
   }
+}
+
+class Tree {
+  constructor () {
+    this.root = null 
+  }
+
+  // reading each level from left to right 
+  traverseBF (fn) {
+    const arr = [this.root]; 
+
+    while (arr.length) {
+      // take out the first element in the array 
+      const node = arr.shift();
+
+      // push all of the node's children into the array using spread operator 
+      arr.push(...node.children); 
+
+      // aplly some function to the node 
+      fn(node);
+    }
+  }
+
 
 }
